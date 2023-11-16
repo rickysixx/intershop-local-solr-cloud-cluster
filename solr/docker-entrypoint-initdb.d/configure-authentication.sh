6f6543f7856ca5ab4d5ccc9139f3b0faf0426282
rickysixx <51907568+rickysixx@users.noreply.github.com>
2023-11-16 11:44:40 +0100

    Add Solr version to container environment variables.

--- solr/docker-entrypoint-initdb.d/configure-authentication.sh
+++ solr/docker-entrypoint-initdb.d/configure-authentication.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-/opt/solr-8.11.1/server/scripts/cloud-scripts/zkcli.sh -zkhost $ZK_HOST -cmd putfile /security.json /docker-entrypoint-initdb.d/security-sample.json
+/opt/solr-${SOLR_VERSION}/server/scripts/cloud-scripts/zkcli.sh -zkhost $ZK_HOST -cmd putfile /security.json /docker-entrypoint-initdb.d/security-sample.json
